fa7303c2350926470c35a6926c3aafed0ad14cc4
estelapedrero <estelapedrero@gmail.com>
2017-08-31 17:36:18 +0200

    input add task

--- src/App.js
+++ src/App.js
@@ -6,26 +6,30 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div>
-          <h1 className={styles.title}>Buenas noches señooooora!</h1>
+        <h1 className={styles.title}>Buenas noches señooooora!</h1>
+        <div className={styles.wrapper}>
             <ul>
               <li>
                 <input type="checkbox" id="todo-1"/>
-                <label for="todo-1">Invitar a Bert a cenar</label>
+                <label htmlFor="todo-1">Invitar a Bert a cenar</label>
               </li>
               <li>
                 <input type="checkbox" id="todo-2"/>
-                <label for="todo-2">Dormir</label>
+                <label htmlFor="todo-2">Dormir</label>
               </li>
               <li>
                 <input type="checkbox" id="todo-3"/>
-                <label for="todo-3">Patinar</label>
+                <label htmlFor="todo-3">Patinar</label>
               </li>
               <li>
                 <input type="checkbox" id="todo-4"/>
-                <label for="todo-4">Llorar</label>
+                <label htmlFor="todo-4">Llorar</label>
               </li>
             </ul>
+            <div className={styles.formbox}>
+              <input type="text" placeholder="add new task"/>
+              <button className={styles.button}>add</button>
+            </div>
         </div>
       </div>
    );
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
+
   devtool: 'cheap-module-eval-source-map',
 
   entry: './src/index.js',
